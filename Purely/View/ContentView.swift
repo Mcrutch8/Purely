b2771722dde96c1c47322f784d152f7e9ea173a2
Mcrutch8 <63677626+Mcrutch8@users.noreply.github.com>
2023-10-30 12:54:20 -0700

    Finished login and registration and now when logged in you go to the Search View

---
+++ Purely/View/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  SearchView.swift
+//  Purely
+//
+//  Created by Max Crutchfield on 10/17/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @EnvironmentObject var authObject: AuthenticationViewModel
+    var body: some View {
+        Group {
+            if authObject.userSession != nil {
+                ProfileView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
--- Purely/View/SearchView.swift
+++ Purely/View/SearchView.swift
@@ -2,21 +2,14 @@
 //  SearchView.swift
 //  Purely
 //
-//  Created by Max Crutchfield on 10/17/23.
+//  Created by Max Crutchfield on 10/30/23.
 //
 
 import SwiftUI
 
 struct SearchView: View {
-    @EnvironmentObject var authObject: AuthenticationViewModel
     var body: some View {
-        Group {
-            if authObject.userSession != nil {
-                ProfileView()
-            } else {
-                LoginView()
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
